d57f7bbc6272038952a68e1b25bef201089403e0
jeonghaemin <jhm0828@gmail.com>
2022-05-02 16:13:53 +0900

    프로젝트 생성

---
+++ src/main/java/com/prgrms/restapiclone/SpringbootRestApiCloneApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.restapiclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestApiCloneApplication.class, args);
+    }
+
+}
